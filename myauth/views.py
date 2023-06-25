554ce256631217c65f7df08049489efbb96149cc
ajit <ajitkrishh@gmail.com>
2023-06-25 20:43:42 +0530

    added jquery support and some prod related setting

--- myauth/views.py
+++ myauth/views.py
@@ -6,29 +6,28 @@ from myauth.forms import CustomUserRegistration
 
 # Create your views here.
 
+
 class My_LoginView(LoginView):
     redirect_authenticated_user = True
 
     def get_success_url(self) -> str:
-        return reverse('topiclist')
-    
+        return reverse("topiclist")
+
 
 def registration(req):
-    print(req , req.POST)
     if req.method == "POST":
         form = CustomUserRegistration(req.POST)
         if form.is_valid():
             form.save()
             return redirect("login")
-        else:  # if invaid form
+        else:
             for field_errors in form.errors.values():
                 for error in field_errors:
-                    print(error)
                     messages.error(req, error)
-            form = CustomUserRegistration(req.POST)
-            return  render(req, "registration/signup.html", {"form": form})
-
+            return render(req, "registration/signup.html", {"form": form})
     elif req.method == "GET":
-
         form = CustomUserRegistration()
-        return render(req, "registration/signup.html", {"form": form})
+        return render(
+            req,
+            "registration/signup.html",
+        )

7f51d559c3da5ddf2b345f81f2e0190852b7d4f2
ajit <ajitkrishh@gmail.com>
2023-07-20 11:47:04 +0530

    commented views for normal views

--- myauth/urls.py
+++ myauth/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
-from .views import CustomTokenObtainPairView, My_LoginView,registration,Registerview
+from .views import CustomTokenObtainPairView,Registerview
 # rest
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
 
 urlpatterns = [
-    path('login',My_LoginView.as_view() , name = 'login'),
-    path('register', registration,name = "register"),
-    path('logout', LogoutView.as_view(),name = "logout"),
+    # path('login',My_LoginView.as_view() , name = 'login'),
+    # path('register', registration,name = "register"),
+    # path('logout', LogoutView.as_view(),name = "logout"),
 
     # rest urls
     path('api/register', Registerview.as_view(),name = "aregister"),
--- myauth/views.py
+++ myauth/views.py
@@ -1,47 +1,19 @@
-from typing import Any, Dict
-from django.shortcuts import redirect, render
-from django.contrib.auth.views import LoginView
-from django.urls import reverse
-from django.contrib import messages
-from myauth.forms import CustomUserRegistration
-from .models import User
+# from typing import Any, Dict
+# from django.shortcuts import redirect, render
+# from django.contrib.auth.views import LoginView
+# from django.urls import reverse
+# from django.contrib import messages
+# from .models import User
 
 # rest_framework
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import CustomTokenObtainPairSerializer, UserRegistrationSerializer
 
 
-
-class My_LoginView(LoginView):
-    redirect_authenticated_user = True
-
-    def get_success_url(self) -> str:
-        return reverse("topiclist")
-   
-
-def registration(req):
-    if req.method == "POST":
-        form = CustomUserRegistration(req.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("login")
-        else:
-            for field_errors in form.errors.values():
-                for error in field_errors:
-                    messages.error(req, error)
-            return render(req, "registration/signup.html", {"form": form})
-
-    elif req.method == "GET":
-        form = CustomUserRegistration()
-        return render(
-            req,
-            "registration/signup.html",
-        )
-
-# for rest register
 class Registerview(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
     permission_classes = (AllowAny,)
@@ -60,11 +32,48 @@ class Registerview(generics.CreateAPIView):
         }
         return Response(response_data)
 
-# views.py
-
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+
+
+
+
+
+
+
+
+
+# for normal views
+# from myauth.forms import CustomUserRegistration
+
+# class My_LoginView(LoginView):
+#     redirect_authenticated_user = True
+
+#     def get_success_url(self) -> str:
+#         return reverse("topiclist")
+   
+
+# def registration(req):
+#     if req.method == "POST":
+#         form = CustomUserRegistration(req.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("login")
+#         else:
+#             for field_errors in form.errors.values():
+#                 for error in field_errors:
+#                     messages.error(req, error)
+#             return render(req, "registration/signup.html", {"form": form})
+
+#     elif req.method == "GET":
+#         form = CustomUserRegistration()
+#         return render(
+#             req,
+#             "registration/signup.html",
+#         )
+
+# for rest apis
 
 
